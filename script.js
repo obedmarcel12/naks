994a1c988222c08c572bd099348d2a344ae6b650
obedgodfrey <obedmarcelcomedian@gmail.com>
2025-09-04 23:04:18 +0100

    commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-      window.addEventListener('scroll', function () {
-        const navbar = document.querySelector('.nav-two')
-        if (window.scrollY > 50) {
-          navbar.classList.add('scrolled')
-        } else {
-          navbar.classList.remove('scrolled')
-        }
-      })
+window.addEventListener('scroll', function () {
+  const navbar = document.querySelector('.nav-two')
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled')
+  } else {
+    navbar.classList.remove('scrolled')
+  }
+})
